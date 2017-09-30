8b48c16321e429f1c2d3b996303c27bb44234b5f
Frank Pommerening <frank@pommerening-online.de>
2017-09-30 21:19:21 +0200

    Add sample console args csharp

---
+++ samples/OpenFAAS/ConsoleArgsCSharp/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test1
+{
+    class Program
+    {
+            private static string[] GetConsoleInput() {
+            var buffer = new  List<string>();
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                buffer.Add(s);
+            }
+            return buffer.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+              var buffer = GetConsoleInput();
+            Console.WriteLine(string.Join(Environment.NewLine, buffer));
+        }
+    }
+}
